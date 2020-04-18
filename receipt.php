235ccff74451e8f5a5998d004a9cf80427082d9f
Noei <35981528+butternoei008@users.noreply.github.com>
2020-04-18 23:16:00 +0700

    Add comment example

--- receipt.php
+++ receipt.php
@@ -15,6 +15,7 @@ function runReceiptCode($prefix, $last_code) {
    }
 }
 
+// Example
 $last_code_query = $conn->query("SELECT rcp_code FROM receipt ORDER BY rcp_code DESC LIMIT 1");
 $last_code = $last_code_query->fetch_assoc();
 $rcp_code = runReceiptCode('SE', $last_code['rcp_code']);
